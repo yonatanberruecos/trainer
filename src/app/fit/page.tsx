e73e097c782e78693fb2863fa65a5f72e2d43ca5
yonatan.berruecos <yonatan.berruecos@gmail.com>
2024-07-31 21:57:32 -0500

    Adding words about exercises

--- src/app/fit/page.tsx
+++ src/app/fit/page.tsx
@@ -65,8 +65,8 @@ export default function fit() {
                             <iframe 
                             width="100%" 
                             height="315" 
-                            // src={`https://www.youtube.com/embed/${videoId}`}
-                            src="https://www.youtube.com/embed/QGTq58winhQ?si=GgnJdMXc86U0TQBO" 
+                            src={`https://www.youtube.com/embed/${videoId}`}
+                            // src="https://www.youtube.com/embed/QGTq58winhQ?si=GgnJdMXc86U0TQBO" 
                             frameBorder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                             allowFullScreen
